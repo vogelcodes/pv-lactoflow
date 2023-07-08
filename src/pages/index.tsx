8dcda45a17d5e87ebab22d25a5a9101ddb5750c0
Daniel <danielcvt@gmail.com>
2023-07-08 13:46:04 -0300

    fb-pixel-fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ const Home: NextPage = () => {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || ""}');
+            fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || ""});
             fbq('track', 'PageView');
             `,
             }}
