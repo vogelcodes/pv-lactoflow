013533759bb288e10ebabaa6ea32159c6d192681
Daniel <danielcvt@gmail.com>
2023-07-12 14:29:59 -0300

    hotjar-script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,6 +61,17 @@ const Home: NextPage = () => {
             fbq('track', 'PageView');
           
           `,}} /> */}
+          <Script id='hj' dangerouslySetInnerHTML={{
+                        __html: `
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3570841,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+                }} />
           <Script
             id="fb-pixel"
             strategy="afterInteractive"
