b59379699355847f046e673ce1c796e5c6c6c3d5
Daniel <danielcvt@gmail.com>
2023-10-20 17:14:36 -0300

    bf23-brach

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,110 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { type NextPage } from "next";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import ptBR from "react-phone-number-input/locale/pt-BR.json";
+import flags from "react-phone-number-input/flags";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+import { api } from "@/utils/api";
+import CTA from "./components/cta";
+import About from "./components/about";
+import Furadeira from "./components/furadeira";
+import Faq from "./components/faq";
+import Footer from "./components/footer";
+import Bonus from "./components/bonus";
 
-export default function Home() {
-  const router = useRouter();
+const Home: NextPage = () => {
+  type E164Number = string | undefined;
 
-  useEffect(() => {
-    // Redirect to /pv2 when the component is mounted
-    void router.push('/pv2');
-  }, []);
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState<E164Number | undefined>();
+  const [email, setEmail] = useState("");
+  const { mutate } = api.example.saveLead.useMutation();
 
-  // Return null or any content you want to display briefly before the redirection happens
-  return null;
-}
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function handleSubmit() {
+    console.log(value);
+
+    mutate({ email, phoneNumber: value?.toString() || "" });
+  }
+
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+
+    <>
+            <Head>
+        <title>LactoFlow®️</title>
+        <meta
+          name="description"
+          content="Método para aumentar a produção de leite materno"
+          />
+        <link rel="icon" href="/favicon.ico" />
+        {/* <link href="https://fonts.cdnfonts.com/css/gotham-6" rel="stylesheet" /> */}
+      </Head>
+      <Script
+        id="hj"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3570841,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                  }}
+                  />
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || ""});
+            fbq('track', 'PageView');
+            `,
+          }}
+      />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6T6HHESNG2"
+        ></Script>
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6T6HHESNG2');`,
+        }}
+        />
+        <h1>Black Friday</h1>
+
+    </>
+        )
+        }
+
+        export default Home;
+  
