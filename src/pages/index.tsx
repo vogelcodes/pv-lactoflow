5e68292d2a9529533cae2b10104f96d60315f9f7
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2023-10-22 19:23:47 +0000

    22-10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ import Faq from "./components/faq";
 import Footer from "./components/footer";
 import Bonus from "./components/bonus";
 import Hotjar from "./components/tags/hotjar";
+import Header1 from "./components/h1";
 
 const Home: NextPage = () => {
   type E164Number = string | undefined;
@@ -57,7 +58,12 @@ const Home: NextPage = () => {
       <Hotjar/>
         <header className="w-full flex items-center h-[5.0625rem] border-b-[1px] border-solid border-[#18191A] sticky top-0 bg-[#020202]">
           <div className="max-w-[80rem] xl:px-0 lg:px-4 mx-auto w-full flex justify-center lg:justify-start ">            
-            <h1>Black Friday<br/>LactoFlow®️</h1>
+          <Image
+              src="/logo-portrait.svg"
+              alt="logo"
+              width={250}
+              height={120}
+            />
           </div>
         </header >
         <div className="w-full text-xl bg-slate">
@@ -68,20 +74,20 @@ const Home: NextPage = () => {
                   Sua amamentação da gestação ao desmame
                   <span className="text-[#03CBD1]"> em um único Curso</span>
                 </h1>
-                <div className="grid gap-2 lg:grid-cols-2 pt-4">
-                  <div className="bg-slate-50 text-slate-700  rounded-md p-2">
+                <div className="grid gap-2 lg:grid-cols-2 pt-4 justify-center">
+                  <div className="bg-slate-50 w-[18rem] text-slate-700  rounded-md p-2">
                       <h2 className="text-center pb-2">Oferta Normal</h2>
                     
                 <ul className="text-sm">
-                  <li className="text-slate-700"><div className="flex justify-between">LactoFlow <span>R$298</span></div></li>
+                  <li className="text-slate-700"><div className="flex justify-between">LactoFlow <span className="text-left w-[3.75rem]">R$298</span></div></li>
                         <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Gestante <span className="text-slate-600">R$200</span></div>
+                          <div className="flex justify-between">WS Gestante <span className="text-slate-600 text-left w-[3.75rem]">R$200</span></div>
                         </li>
                         <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Intro Alimentar  <span className="text-slate-600">R$200</span></div>
+                          <div className="flex justify-between">WS Intro Alimentar  <span className="text-slate-600 text-left w-[3.75rem]">R$200</span></div>
                         </li>
                         <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Desmame  <span className="text-slate-600">R$200</span></div>
+                          <div className="flex justify-between">WS Desmame  <span className="text-slate-600 text-left w-[3.75rem]">R$200</span></div>
                         </li>
                         <li className="pt-4">
                           <div className="flex justify-between">Total: <span className="text-lg">R$1.097,00</span></div>
@@ -89,29 +95,45 @@ const Home: NextPage = () => {
                 </ul>
 
                   </div>
-                  <div className="bg-slate-50 text-slate-700  rounded-md p-2">
-                      <h2 className="text-center pb-2">Oferta Black Friday</h2>
+                  <div className="bg-gray-950 w-[18rem] text-slate-50  rounded-md p-2">
+                      <h2 className="text-center pb-2">Oferta <span className="text-[#0f0]">Black Friday</span></h2>
                     
                 <ul className="text-sm">
-                  <li className="text-slate-700"><div className="flex justify-between">LactoFlow <span>R$298</span></div></li>
-                        <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Gestante <span className="line-through text-slate-400">R$200</span></div>
+                  <li className="text-slate-50"><div className="flex justify-between">LactoFlow <span className="text-left w-[3.75rem]">R$298</span></div></li>
+                        <li className="pl-2 text-slate-50">
+                          <div className="flex justify-between">WS Gestante <span className="line-through text-slate-400 text-left w-[3.75rem]">R$200</span></div>
                         </li>
-                        <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Intro Alimentar  <span className="line-through text-slate-400">R$200</span></div>
+                        <li className="pl-2 text-slate-50">
+                          <div className="flex justify-between">WS Intro Alimentar  <span className="line-through text-slate-400 text-left w-[3.75rem]">R$200</span></div>
                         </li>
-                        <li className="pl-2 text-slate-600">
-                          <div className="flex justify-between">WS Desmame  <span className="line-through text-slate-400">R$200</span></div>
+                        <li className="pl-2 text-slate-50">
+                          <div className="flex justify-between">WS Desmame  <span className="line-through text-slate-400 text-left w-[3.75rem]">R$200</span></div>
                         </li>
                         <li className="pt-4">
-                          <div className="flex justify-between">Total: <span className="text-lg">R$298</span></div>
+                          <div className="flex justify-between">Total: <span className="text-lg text-[#0f0]">R$298</span></div>
                         </li>
                 </ul>
 
                   </div>
                 </div>
               </div>
+              <Image width={300} height={0} alt="Carolina Procaci" className="rounded-md" src={"/carol.webp"}/>
+
             </div>
+            <div className="card bg-cream max-w-2xl shadow-lg text-blue">
+  <div className="card-body">
+            <h1 className="text-left mb-[14.4px] mt-3 px-4 text-[25px] font-extrabold leading-[29.17px] tracking-[-25] sm:px-2 lg:text-[31.25px]">
+                LactoFlow               
+            </h1>
+
+            <ul className="flex flex-col text-xl gap-2 m-2">
+            <div className="flex gap-2">🎓 <li>Carolina Procaci é biomédica formada na Universidade Federal do Estado do Rio de Janeiro</li></div>
+            <div className="flex gap-2">🏥 <li>Consultora em amamentação pelo único curso reconhecido pelo MEC</li></div>
+            <div className="flex gap-2">👩‍👧‍👦 <li>Mãe de 2 filhos e lactante há mais de 5 anos.</li></div>
+            </ul>
+            </div>
+            </div>
+
           </div>
 
         </div>
