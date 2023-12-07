f519ab4cb251ad5c627fe73d3740096d194da855
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2023-12-07 07:38:23 +0000

    retorno-ao-trabalho-page

--- src/pages/retorno-ao-trabalho.tsx
+++ src/pages/retorno-ao-trabalho.tsx
@@ -17,6 +17,7 @@ import Furadeira from "./components/furadeira";
 import Faq from "./components/faq";
 import Footer from "./components/footer";
 import Bonus from "./components/bonus";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   type E164Number = string | undefined;
@@ -24,19 +25,31 @@ const Home: NextPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState<E164Number | undefined>();
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [cta, setCta] = useState("");
   const { mutate } = api.example.saveLead.useMutation();
+  const router = useRouter();
 
   function closeModal() {
     setIsOpen(false);
   }
 
-  function openModal() {
+  function openModal(option: string) {
+    setCta(option);
+    console.log(cta);
     setIsOpen(true);
   }
   function handleSubmit() {
     console.log(value);
+    console.log(router.asPath);
 
-    mutate({ email, phoneNumber: value?.toString() || "" });
+    mutate({
+      ctaOption: cta,
+      url: router.asPath,
+      name,
+      email,
+      phoneNumber: value?.toString() || "",
+    });
   }
 
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -50,7 +63,6 @@ const Home: NextPage = () => {
           content="Método para aumentar a produção de leite materno"
         />
         <link rel="icon" href="/favicon.ico" />
-        {/* <link href="https://fonts.cdnfonts.com/css/gotham-6" rel="stylesheet" /> */}
       </Head>
       <Script
         id="hj"
@@ -99,162 +111,218 @@ const Home: NextPage = () => {
            gtag('config', 'G-6T6HHESNG2');`,
         }}
       />
-        <div className="flex flex-col items-center justify-center bg-blue py-2 text-cream sm:py-4">
-          <div className="w-[180px] sm:w-[300px]">
-            <Image
-              src="/logo-portrait.svg"
-              alt="logo"
-              width={300}
-              height={120}
-            />
-          </div>
-          <h4 hidden className="text-center text-3xl text-[#fcfaef]">
-            Conheça o Método
-          </h4>
-          <h1
-            hidden
-            className="text-center text-5xl font-extrabold tracking-tight text-cream sm:text-[5rem]"
-          >
-            Lacto<span className="text-green">Flow</span>{" "}
-          </h1>
-          <h1 className="mb-[14.4px] mt-6 px-4 text-center text-[18px] font-extrabold uppercase leading-[29px] tracking-[-25] text-cream sm:w-2/3 sm:px-0 sm:text-[24px] sm:leading-[36px]">
+
+      <Script
+        id="hotmart_launcher_script"
+        dangerouslySetInnerHTML={{
+          __html: `(function(l,a,u,n,c,h,e,r){l['HotmartLauncherObject']=c;l[c]=l[c]||function(){
+			(l[c].q=l[c].q||[]).push(arguments)},l[c].l=1*new Date();h=a.createElement(u),
+			e=a.getElementsByTagName(u)[0];h.async=1;h.src=n;e.parentNode.insertBefore(h,e)
+			})(window,document,'script','//launcher.hotmart.com/launcher.js','hot');
+
+			hot('account','32aba353-3c4c-3d94-9d63-eb652a9ca8ee');`,
+        }}
+      />
+
+      <div className="flex flex-col items-center justify-center bg-blue py-2 text-cream sm:py-4">
+        <div className="w-[180px] sm:w-[300px]">
+          <Image src="/logo-portrait-v2.svg" alt="logo" width={300} height={120} />
+        </div>
+        <h4 hidden className="text-center text-3xl text-[#fcfaef]">
+          Conheça o Método
+        </h4>
+        <h1
+          hidden
+          className="text-center text-5xl font-extrabold tracking-tight text-cream sm:text-[5rem]"
+        >
+          Lacto<span className="text-green">Flow</span>{" "}
+        </h1>
+        <h1 className="mb-[14.4px] mt-6 px-4 text-center text-[18px] font-extrabold uppercase leading-[29px] tracking-[-25] text-cream sm:w-2/3 sm:px-0 sm:text-[24px] sm:leading-[36px]">
             Monte seu estoque de{" "}
             <br/><span className="text-[25px] mx-2 text-red-400 sm:text-[35px]">
               leite materno
             </span><br/>
             e volte ao trabalho{" "}
             <span className="text-[25px] text-green sm:text-[35px]">
-              traquila.
+              tranquila.
             </span>{" "}
             
           </h1>
-          <p className="mx-auto w-[80%] max-w-[600px] text-center text-[16.67px] font-bold leading-[22.9px] tracking-[-25] sm:mt-7 sm:px-2 lg:mt-5 lg:text-[18.75px]">
-          Assista esse vídeo e comece a aumentar a sua produção de leite agora mesmo com as técnicas:
-          </p>
-          <div className="flex  flex-col lg:flex-row items-center">
-            <div className="flex items-center">
-
-            <div className="lg:-rotate-90 m-2">
-
-            <Image
-              className="ml-2 animate-bounce"
-              src="/arrow-down.svg"
-              alt="one"
-              height={36}
-              width={36}
+        <p className="mx-auto w-[80%] max-w-[600px] text-center text-[16.67px] font-bold leading-[22.9px] tracking-[-25] sm:mt-7 sm:px-2 lg:mt-5 lg:text-[18.75px]">
+          Assista esse vídeo e comece a aumentar a sua produção de leite agora
+          mesmo com as técnicas:
+        </p>
+        <div className="flex  flex-col items-center lg:flex-row">
+          <div className="flex items-center">
+            <div className="m-2 lg:-rotate-90">
+              <Image
+                className="ml-2 animate-bounce"
+                src="/arrow-down.svg"
+                alt="one"
+                height={36}
+                width={36}
               />
-              </div>
-              </div>
+            </div>
+          </div>
 
-          <div className="mx-auto px-2 grid lg:grid-cols-1 grid-cols-2 gap-3 uppercase text-green">
+          <div className="mx-auto grid grid-cols-2 gap-3 px-2 uppercase text-green lg:grid-cols-1">
             <div className="mt-1 flex items-center gap-x-2 lg:mt-4">
-              <div className="border-green border-[2px] rounded-full w-8 h-8 flex items-center justify-center">
-                
-                  <span className="text-2xl text-cream ">1</span>
-                
+              <div className="flex h-8 w-8 items-center justify-center rounded-full border-[2px] border-green">
+                <span className="text-2xl text-cream ">1</span>
               </div>
               <p className="text-[15.67px] font-medium leading-[22.9px] tracking-[-25] lg:text-[16.75px]">
                 Meia Velha
               </p>
             </div>
             <div className=" flex items-center gap-x-2 lg:mt-4">
-            <div className="border-green border-[2px] rounded-full w-8 h-8 flex items-center justify-center">
-                
+              <div className="flex h-8 w-8 items-center justify-center rounded-full border-[2px] border-green">
                 <span className="text-2xl text-cream">2</span>
-              
               </div>
               <p className="text-[15.67px] font-medium leading-[22.9px] tracking-[-25] lg:text-[16.75px]">
                 Distração
               </p>
             </div>
             <div className=" flex items-center gap-x-2 lg:mt-4">
-            <div className="border-green border-[2px] rounded-full w-8 h-8 flex items-center justify-center">
-                
+              <div className="flex h-8 w-8 items-center justify-center rounded-full border-[2px] border-green">
                 <span className="text-2xl text-cream">3</span>
-              
               </div>
               <p className="text-[15.67px] font-medium leading-[22.9px] tracking-[-25] lg:text-[16.75px]">
-              5-5
+                5-5
               </p>
             </div>
             <div className=" flex items-center gap-x-2 lg:mt-4">
-            <div className="border-green border-[2px] rounded-full w-8 h-8 flex items-center justify-center">
-                
+              <div className="flex h-8 w-8 items-center justify-center rounded-full border-[2px] border-green">
                 <span className="text-2xl text-cream">4</span>
-              
               </div>
               <p className="text-[15.67px] font-medium leading-[22.9px] tracking-[-25] lg:text-[16.75px]">
-              20-max
+                20-max
               </p>
             </div>
-            </div>
           </div>
-          {/* <iframe className="w-full aspect-video rounded-md mt-8 mx-auto max-w-[22rem] lg:max-w-[53.25rem]" src="https://youtube.com/embed/IVKkQA9p7go"></iframe> */}
-          <div className="mx-auto my-8 aspect-video w-full lg:max-w-[53.25rem]">
-            <LiteYouTubeEmbed
-              id="eXp3-bclWPs"
-              title="Método LactoFlow®️"
-              poster="maxresdefault"
-            />
-          </div>
+        </div>
+        {/* <iframe className="w-full aspect-video rounded-md mt-8 mx-auto max-w-[22rem] lg:max-w-[53.25rem]" src="https://youtube.com/embed/IVKkQA9p7go"></iframe> */}
+        <div className="mx-auto my-8 aspect-video w-full lg:max-w-[53.25rem]">
+          <LiteYouTubeEmbed
+            id="eXp3-bclWPs"
+            title="Método LactoFlow®️"
+            poster="maxresdefault"
+          />
+        </div>
 
-          <CTA openModal={openModal} />
-          {/* <Image alt="checklist" src={"/checklist.png"} height={762} width={623}></Image> */}
+        <CTA ctaOption="1" openModal={openModal} />
+        {/* <Image alt="checklist" src={"/checklist.png"} height={762} width={623}></Image> */}
 
-          <Furadeira />
-          {/* <section>
+        <Furadeira />
+        {/* <section>
 
 <h1 className="font-extrabold text-[25px] lg:text-[31.25px] text-center mt-6 leading-[29.17px] tracking-[-25] mb-[14.4px] ">
 Explicação do método
 </h1>
 </section> */}
-          <section className="my-2">
-            <h1 className="mb-[14.4px] mt-6 px-4 text-center text-[25px] font-extrabold leading-[29.17px] tracking-[-25] sm:px-2 lg:text-[31.25px]">
-              Depoimento das alunas que seguiram o método LactoFlow®️:
-            </h1>
-            <div className="flex flex-col items-center gap-4 md:grid lg:grid-cols-3 md:grid-cols-2">
-              {[1, 2, 3, 4, 5, 6].map((number) => {
-                return (
-                  <div className="" key={number}>
-                    <Image
-                      className="rounded-xl mx-auto max-w-[250px]"
-                      key={number}
-                      width={300}
-                      height={500}
-                      src={`/depoimentos/metodo/${number}.jpg`}
-                      alt="depoimento"
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </section>
-          <CTA
-            openModal={openModal}
-            label="Também quero esses resultados"
-            
+        <section className="my-2">
+          <h1 className="mb-[14.4px] mt-6 px-4 text-center text-[25px] font-extrabold leading-[29.17px] tracking-[-25] sm:px-2 lg:text-[31.25px]">
+            Depoimento das alunas que seguiram o método LactoFlow®️:
+          </h1>
+          <div className="flex flex-col items-center gap-4 md:grid md:grid-cols-2 lg:grid-cols-3">
+            <div className="w-[340px]">
+          <LiteYouTubeEmbed
+              id="PzxmWmJLjz8"
+              title="Método LactoFlow®️"
+              poster="maxresdefault"
+              thumbnail="/depoimentos/depo-priscila.webp"
+              aspectHeight={500}
+              aspectWidth={280}
             />
-          <About/>
-          <Faq />
-          <Bonus/>
-          <CTA
-            openModal={openModal}
-            label="Quero aumentar minha produção de leite"
-            price
+
+                </div>
+          <div className="w-[340px]">
+          <LiteYouTubeEmbed
+              id="HC6maLeH-iM"
+              title="Método LactoFlow®️"
+              poster="maxresdefault"
+              thumbnail="/depoimentos/depo-raquel.webp"
+              aspectHeight={500}
+              aspectWidth={280}
             />
-          <Footer>
-          <div className="flex flex-col sm:flex-row text-center sm:text-left">
-            <span>Copyright © 2023</span><span className="hidden sm:block mx-3">|</span><span>Todos os direitos reservados.</span>
+
+                </div>
+            {[1, 2].map((number) => {
+              return (
+                <div className="" key={number}>
+                  <Image
+                    className="mx-auto rounded-xl"
+                    key={number}
+                    width={340}
+                    height={500}
+                    src={`/depoimentos/metodo/${number}.jpg`}
+                    alt="depoimento"
+                  />
+                </div>
+              );
+            })}
+            {[ 3, 4, 5, 6].map((number) => {
+              return (
+                <div className="" key={number}>
+                  <Image
+                    className="mx-auto rounded-xl"
+                    key={number}
+                    width={340}
+                    height={500}
+                    src={`/depoimentos/metodo/${number}.jpg`}
+                    alt="depoimento"
+                  />
+                </div>
+              );
+            })}
           </div>
-          </Footer>
-          {/* <div className="mx-auto max-w-[19.875rem] lg:max-w-[53.25rem] ">
+        </section>
+
+        <Bonus />
+        <CTA
+          ctaOption="2"
+          openModal={openModal}
+          label="Tenha acesso a tudo isso agora"
+        />
+        <About />
+        <Faq />
+        <CTA
+          ctaOption="3"
+          openModal={openModal}
+          label="Quero aumentar minha produção de leite"
+          price
+        />
+        <Footer>
+          <div className="flex flex-col text-center sm:flex-row sm:text-left">
+            <span>Copyright © 2023</span>
+            <span className="mx-3 hidden sm:block">|</span>
+            <span>Todos os direitos reservados.</span>
+          </div>
+        </Footer>
+        {/* <div className="mx-auto max-w-[19.875rem] lg:max-w-[53.25rem] ">
               <div className="w-full border-2 border-[#4E859E] bg-[#EDF4FA] lg:bg-[#EDF4FA] rounded-lg flex gap-x-4 py-9 lg:py-6 px-3 mt-10 items-center justify-center"><div className="relative w-[32px] h-[30px]"><span><img alt="" src="https://vendatodosantodia.com.br//imgPg/icon-alert-blue.svg?auto=format&amp;fit=max&amp;w=3840" decoding="async" data-nimg="fill"  loading="lazy"/></span></div><p className="text-[16.17px] text-[#4E859E] font-regular max-w-[80%]">Com o Appbumper é mais fácil fazer o que tem que ser feito e do jeito que tem que ser feito. É quase impossível errar e você nunca fica perdido, sem saber o que fazer.</p>
               </div>
           </div> */}
-          {/* <CTA openModal={openModal} price /> */}
-              <div className=" fixed right-5 bottom-5 cursor-pointer"><div className="relative w-[60px] h-[60px]"><a target="_blank" href="https://wa.me/5521986388688?text=Oi%2C+tudo+bem%3F+Eu+tenho+uma+pergunta+sobre+o+curso+LactoFlow."><span><Image width={60} height={60} alt="whats logo" src="/whatsapp-logo.svg" decoding="async" data-nimg="fill" /></span></a></div></div>
-              <div className="">
-
+        {/* <CTA openModal={openModal} price /> */}
+        <div className=" fixed bottom-5 right-5 cursor-pointer">
+          <div className="relative h-[60px] w-[60px]">
+            <a
+              target="_blank"
+              href="https://wa.me/5521986388688?text=Oi%2C+tudo+bem%3F+Eu+tenho+uma+pergunta+sobre+o+curso+LactoFlow."
+            >
+              <span>
+                <Image
+                  width={60}
+                  height={60}
+                  alt="whats logo"
+                  src="/whatsapp-logo.svg"
+                  decoding="async"
+                  data-nimg="fill"
+                />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="">
           <Transition appear show={isOpen} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={closeModal}>
               <Transition.Child
@@ -294,7 +362,16 @@ Explicação do método
                         Preencha esse formulário e entre para o LactoFlow
                       </Dialog.Title>
                       <div className="mt-2">
-                        <form className="mb-8  text-blue flex flex-col">
+                        <form className="mb-8  flex flex-col text-blue">
+                          <label htmlFor="celular">Nome</label>
+                          <input
+                            className="pl-2 dark:bg-cream"
+                            type="text"
+                            name="email"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Nome"
+                          />
                           <label htmlFor="celular">Email</label>
                           <input
                             className="pl-2 dark:bg-cream"
@@ -303,30 +380,33 @@ Explicação do método
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="maeincrivel@email.com"
-                            />
+                          />
                           <label htmlFor="celular">Celular</label>
                           <PhoneInput
-                          className=""
+                            className="bg-cream"
                             labels={ptBR}
                             flags={flags}
                             value={value}
                             defaultCountry="BR"
                             onChange={setValue}
-                            />
+                          />
                         </form>
                         <a
                           target="_blank"
                           href={`https://pay.hotmart.com/O84147403X?email=${email}&phoneac=${
                             value?.toString() || ""
-                          }`}
-                          >
+                          }&name=${name}`}
+                        >
                           <button
                             onClick={handleSubmit}
                             className="mx-auto rounded-lg border-b-4 border-b-[#236C0F] bg-[#40C351] px-2 py-3 text-[13.6px] font-extrabold uppercase text-cream hover:scale-[104%] hover:border-b-[#44972d] hover:bg-[#236C0F] lg:py-5 lg:text-[22.6px]"
-                            >
+                          >
                             Quero aumentar minha produção de leite
                           </button>
                         </a>
+                        <h3 className="text-center text-base text-blue">
+                          SEUS DADOS ESTÃO SEGUROS
+                        </h3>
                       </div>
                     </Dialog.Panel>
                   </Transition.Child>
@@ -334,8 +414,8 @@ Explicação do método
               </div>
             </Dialog>
           </Transition>
-                            </div>
         </div>
+      </div>
     </>
   );
 };
