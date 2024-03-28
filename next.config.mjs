35e22eecbfba543de9ea53ab0539d02438783b36
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2024-03-28 07:39:34 +0000

    captain+dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -24,5 +24,10 @@ const config = {
     locales: ["br"],
     defaultLocale: "br",
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 export default config;
