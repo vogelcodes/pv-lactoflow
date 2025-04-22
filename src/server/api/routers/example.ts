d2b73cb652744ee2273105bba1ffcae7c1311198
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2025-04-22 19:29:09 +0000

    mchimp-diaMaes

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -55,6 +55,10 @@ export const exampleRouter = createTRPCRouter({
         "https://project.us13.list-manage.com/subscribe/post?u=f2c713cb04fc024e126ae662d&amp;id=96c32b9825&amp;f_id=0034ede7f0";
 
       const formData = new FormData();
+      const space = input.name.indexOf(" ");
+      const firstName = input.name.split(" ")[0] || "";
+      const lastName = input.name.slice(space + 1) || "";
+
       formData.append("name", input.name);
       formData.append("email", input.email);
       formData.append("phone", input.phoneNumber);
@@ -67,19 +71,20 @@ export const exampleRouter = createTRPCRouter({
       formData.append("ip", input.ip || "");
       formData.append("agent", input.agent || "");
       const mcForm = new FormData();
-      mcForm.append("FNAME", input.name);
+      mcForm.append("FNAME", firstName);
+      mcForm.append("LNAME", lastName);
       mcForm.append("EMAIL", input.email);
       mcForm.append("PHONE", input.phoneNumber);
-      mcForm.append("tags", "7067103");
+      mcForm.append("tags", "7217473");
       mcForm.append("b_f2c713cb04fc024e126ae662d_96c32b9825", "");
 
       if (process.env.NODE_ENV !== "development") {
-        const [telegramMessage, gSheets] = await Promise.all([
-          // fetch(mailchimpUrl, {
-          //   method: "POST",
-          //   headers: {},
-          //   body: mcForm,
-          // }),
+        const [mchimp, telegramMessage, gSheets] = await Promise.all([
+          fetch(mailchimpUrl, {
+            method: "POST",
+            headers: {},
+            body: mcForm,
+          }),
           fetch(
             `https://api.telegram.org/bot6798939077:AAEhMt8W_okiJ1PYw4ySWyUxRG-uHTP7a_8/sendMessage?chat_id=-1002059061283&text=${encodeURIComponent(
               `Nova Lead:\n${input.name}\n${input.email}\n${
